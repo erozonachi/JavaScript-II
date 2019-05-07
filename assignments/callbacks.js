366f5921edc1d2777272444b534a37d7a323a2b2
erozonachi <erozonachi.james@gmail.com>
2019-05-07 18:06:54 +0100

    Create a higher order function - stretch
    Create a called `removeDuplicates` that removes all duplicate values
    from a given array parameter and pass the duplicate free array to a
    callback function parameter.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -75,4 +75,9 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const uniqueList = new Set(array);
+  return cb([...uniqueList]);
 }
+
+// removeDuplicate() invocation
+removeDuplicates([1,2,3,3,3,4,5], (distinctList) => console.log(distinctList));
